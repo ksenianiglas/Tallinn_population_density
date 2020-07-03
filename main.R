d8361b4c12c20c4967199d71ec389ee96d364f57
Ksenia <50526165+ksenianiglas@users.noreply.github.com>
2020-07-03 19:06:56 +0300

    final code

--- main.R
+++ main.R
@@ -1,1 +1,19 @@
-library(rgdal)
+library(sf)
+library(tidyverse)
+
+# border data: https://www.tallinn.ee/est/ehitus/Tallinna-linnaosade-ja-asumite-piirid
+df_TLN <- st_read("asumid/t02_41_asum.shp")
+
+#lave out Aegna saar (sorry Aegna but you're too far and too uninhabited) 
+df_TLN <- df_TLN %>%
+  filter(asumi_nimi != "Aegna") %>%
+  select(asumi_nimi, linnaosa_n, geometry)
+
+# population size data: https://www.tallinn.ee/est/Tallinn-arvudes
+df_TLN <- df_TLN %>%
+  left_join(read_csv("pindala_ja_rahvaarv.csv"),
+            by = c('asumi_nimi' = 'asum')) %>%
+  mutate(pop_density = rahvaarv/pindala)
+
+ggplot(df_TLN) +
+  geom_sf(aes(fill = pop_density))
